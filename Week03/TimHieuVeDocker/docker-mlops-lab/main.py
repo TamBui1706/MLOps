c2f1459e0dd0dc143eee7fac86d77a67f4cb22ab
Bui Le Hong Tam <builehongtamk16@siu.edu.vn>
2025-11-22 11:03:26 +0700

    Add files and report

---
+++ Week03/TimHieuVeDocker/docker-mlops-lab/main.py
@@ -0,0 +1,8 @@
+# main.py
+import time
+
+print("=== MLOps Lab: Docker Training ===")
+print("Step 1: Load Data... Done")
+print("Step 2: Training Model... Done")
+print("Step 3: Model Accuracy: 80%")
+print("Hello from Docker Container!")
